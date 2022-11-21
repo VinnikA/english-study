7d58a389681ceb7c47e90e45e95cc505a38a2df7
VinnikA <vamposht@gmail.com>
2022-11-21 14:06:58 +0200

    fix: feedbacks image added

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,9 +1,13 @@
 import "./styles.scss"
+import { useState } from "react"
 import { feedbacks } from "../../content/content"
 import { IconBtn } from "../ui/icon-btn"
 import arrowLeft from "../../assets/img/icons/arrow-left.svg"
 import arrowRight from "../../assets/img/icons/arrow-right.svg"
-import { useState } from "react"
+import img0 from "../../assets/img/feedbacks/1.png"
+import img1 from "../../assets/img/feedbacks/2.png"
+import img2 from "../../assets/img/feedbacks/3.png"
+
 
 
 export const Carousel = () => {
@@ -39,22 +43,48 @@ export const Carousel = () => {
           <img src={arrowLeft} alt="go back" />
         </IconBtn>
         <div className="carousel__screen">
-
-          {feedbacks.map((item, index) => (
-            <div className={`testimonial testimonial_${position(index)}`}>
+          
+          <div className={`testimonial testimonial_${position(0)}`}>
             <div className="testimonial__img">
-              <img src={item.imgUrl} />
+              <img src={img0} />
             </div>
             <div className="testimonial__content">
               <p className="testimonial__text">
-                {item.feedback}
+                {feedbacks[0].feedback}
               </p>
               <p className="testimonial__author">
-                {item.author}, {item.city}
+                {feedbacks[0].author}, {feedbacks[0].city}
               </p>
             </div>
           </div>
-          ))}
+
+          <div className={`testimonial testimonial_${position(1)}`}>
+            <div className="testimonial__img">
+              <img src={img1} />
+            </div>
+            <div className="testimonial__content">
+              <p className="testimonial__text">
+                {feedbacks[1].feedback}
+              </p>
+              <p className="testimonial__author">
+                {feedbacks[1].author}, {feedbacks[1].city}
+              </p>
+            </div>
+          </div>
+
+          <div className={`testimonial testimonial_${position(2)}`}>
+            <div className="testimonial__img">
+              <img src={img2} />
+            </div>
+            <div className="testimonial__content">
+              <p className="testimonial__text">
+                {feedbacks[2].feedback}
+              </p>
+              <p className="testimonial__author">
+                {feedbacks[2].author}, {feedbacks[2].city}
+              </p>
+            </div>
+          </div>
 
         </div>
         <IconBtn clickFn={goForward}>
